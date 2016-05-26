b21c108d3270b4179aae67127f800f4f346d0a27
juliendauger <julien.dauger@hec.edu>
2016-05-26 15:59:00 +0200

    Première version JS : code de mousse

--- client/main.js
+++ client/main.js
@@ -1,11 +1,6 @@
-
-
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
-
 import './main.html';
 
-/* end google maps -----------------------------------------------------*/
+
 
 
 $("#bars").click(function(){
